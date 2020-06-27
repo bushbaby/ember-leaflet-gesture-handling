20571eb0df7cd3561d7c7c5528a5e5a70dd9db72
Bas Kamer <bas@bushbaby.nl>
2020-06-27 14:45:16 +0200

    disabled overriding config to keep defaults

--- config/environment.js
+++ config/environment.js
@@ -5,12 +5,12 @@ module.exports = function (/* environment, appConfig */) {
     ENV: {
       leafletGestureHandling : {
         enabled :true,
-        text: {
-          touch: "Hai, use two fingers to move the map",
-          scroll: "Hai, use ctrl + scroll to zoom the map",
-          scrollMac: "Hait, use \u2318 + scroll to zoom the map"
-        },
-        duration: 2000
+        // text: {
+        //   touch: "Hai, use two fingers to move the map",
+        //   scroll: "Hai, use ctrl + scroll to zoom the map",
+        //   scrollMac: "Hai, use \u2318 + scroll to zoom the map"
+        // },
+        // duration: 2000
       }
     }
   };
